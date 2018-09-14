bbc0464e5c1a5738c4c3c2713588dacd68d728ef
Kevin Thrailkill <kevinjt92@gmail.com>
2018-09-14 16:39:16 -0700

    minor tweak

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var httpProxy = require('http-proxy');
 
-var apiForwardingUrl = 'https://api.yelp.com/v3/graphql';
+var apiForwardingUrl = 'https://api.yelp.com/v3';
 
 var server = express();
 server.set('port', 5000);
@@ -21,18 +21,18 @@ var apiProxy = httpProxy.createProxyServer(proxyOptions);
 console.log('Forwarding API requests to ' + apiForwardingUrl);
 
 server.all('/graphql', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', [
+    'POST',
+    'GET',
+    'OPTIONS',
+    'PUT',
+    'DELETE'
+  ]);
 
-  res.setHeader(
+  res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-
-  res.setHeader('Access-Control-Allow-Methods', ['POST', 'GET', 'OPTIONS']);
-
-  res.setHeader(
-    'Authorization',
-    'Bearer zZcR-ZhvOyXrlHQ1nCuPxQMdFXTjolxR4JWxua-Rq4geBcFklVuTA2fOuj04Jghi7gbIka7PUOmIFignaXFSBLkzzrbA--qxXmGYbYvt8Nn1052tlNPTFHDC-bb3WXYx'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, Referer, User-Agent'
   );
 
   apiProxy.web(req, res, { target: apiForwardingUrl });
